5207e963bb9c4278623cb217b4386882d8e62e97
yvonneyx <zhuyuxin0627@gmail.com>
2021-07-11 17:34:31 +0200

    parse tag

--- toy-browser/html-parse/parser.js
+++ toy-browser/html-parse/parser.js
@@ -1,6 +1,75 @@
 const EOF = Symbol("EOF"); // EOF: End Of File
 
-function data(c) {}
+function data(c) {
+  if (c == "<") {
+    return tagOpen;
+  } else if (c == EOF) {
+    return;
+  } else {
+    return data;
+  }
+}
+
+function tagOpen(c) {
+  if (c == "/") {
+    return endTagOpen;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName(c); // reconsume c
+  } else {
+    return;
+  }
+}
+
+function endTagOpen(c) {
+  //自封闭标签
+  if (c == ">") {
+  }
+  //结束标签
+  else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "endTag",
+      tagName: "",
+    };
+    return tagName(c);
+  } else if (c == EOF) {
+  } else {
+  }
+}
+
+function tagName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c == "/") {
+    return selfClosingStartTag;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName;
+  } else if (c == ">") {
+    return data;
+  } else {
+    return tagName;
+  }
+}
+
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c == ">") {
+    return data;
+  } else if (c == "=") {
+    return beforeAttributeName;
+  } else {
+    return beforeAttributeName;
+  }
+}
+
+function selfClosingStartTag(c) {
+  if (c == ">") {
+    currentToken.isSelfClosing = true;
+    return data;
+  } else if (c == EOF) {
+  } else {
+  }
+}
 
 module.exports.parseHTML = function parseHTML(html) {
   const state = data;
