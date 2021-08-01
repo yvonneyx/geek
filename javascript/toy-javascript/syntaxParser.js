571519e17557568b4d3fcef0f6f2d5af9342e5e2
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-01 12:15:57 +0200

    JS根据语法树实现语法分析一

--- javascript/toy-javascript/syntaxParser.js
+++ javascript/toy-javascript/syntaxParser.js
@@ -38,3 +38,48 @@ let syntax = {
   PrimaryExpression: [["(", "Expression", ")"], ["Literal"], ["Identifier"]],
   Literal: [["Number"]],
 };
+
+// 避免死循环，利用回环来实现无限状态机
+let hash = {};
+
+// 第一层closure
+function closure(state) {
+  hash[JSON.stringify(state)] = state;
+  let queue = [];
+  for (let symbol in state) {
+    queue.push(symbol);
+  }
+  while (queue.length) {
+    let symbol = queue.shift();
+    // console.log(symbol);
+    if (syntax[symbol]) {
+      for (let rule of syntax[symbol]) {
+        if (!state[rule[0]]) queue.push(rule[0]);
+        let current = state;
+        for (let part of rule) {
+          if (!current[part]) current[part] = {};
+          current = current[part]; //下移一格
+        }
+        current.$isRuleEnd = true;
+      }
+    }
+  }
+  for (let symbol in state) {
+    if (hash[JSON.stringify(state[symbol])]) {
+      state[symbol] = hash[JSON.stringify(state[symbol])];
+    } else {
+      closure(state[symbol]);
+    }
+  }
+}
+
+let end = {
+  $isEnd: true,
+};
+
+let start = {
+  // Program: end,
+  IfStatement: end,
+};
+
+closure(start);
