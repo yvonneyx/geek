0701068e14ef34e5c3064e9b63c5a02f9777219c
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-20 20:59:45 +0200

    组件化-实现自定义JSX基本使用

--- component/jsx/webpack.config.js
+++ component/jsx/webpack.config.js
@@ -8,7 +8,12 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
-            plugins: [["@babel/plugin-transform-react-jsx"]],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                { pragma: "createElement" },
+              ],
+            ],
           },
         },
       },
