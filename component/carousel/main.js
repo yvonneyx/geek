9d79f2580e200e82025f6fd1d434476b970c18ee
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-21 12:56:51 +0200

    组件化-实现自动轮播组件

--- component/carousel/main.js
+++ component/carousel/main.js
@@ -12,11 +12,34 @@ class Carousel extends Component {
 
   render() {
     this.root = document.createElement("div");
+    this.root.classList.add("carousel");
     for (let record of this.attributes.src) {
-      let child = document.createElement("img");
-      child.src = record;
+      let child = document.createElement("div");
+      child.style.backgroundImage = `url(${record})`;
       this.root.appendChild(child);
     }
+
+    let currentIndex = 0;
+
+    setInterval(() => {
+      let children = this.root.children;
+      let nextIndex = (currentIndex + 1) % children.length;
+
+      let current = children[currentIndex];
+      let next = children[nextIndex];
+
+      next.style.transition = "none";
+      next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+      setTimeout(() => {
+        next.style.transition = "ease .5s";
+        current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+        next.style.transform = `translateX(${-nextIndex * 100}%)`;
+
+        currentIndex = nextIndex;
+      }, 16);
+    }, 1500);
+
     return this.root;
   }
 
@@ -26,11 +49,11 @@ class Carousel extends Component {
 }
 
 let images = [
-  "https://images.pexels.com/photos/8678811/pexels-photo-8678811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  "https://images.pexels.com/photos/5847383/pexels-photo-5847383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  "https://images.pexels.com/photos/6626710/pexels-photo-6626710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  "https://images.pexels.com/photos/3632966/pexels-photo-3632966.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  "https://images.pexels.com/photos/4376212/pexels-photo-4376212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://images.pexels.com/photos/8678811/pexels-photo-8678811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
+  "https://images.pexels.com/photos/5847383/pexels-photo-5847383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
+  "https://images.pexels.com/photos/6626710/pexels-photo-6626710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
+  "https://images.pexels.com/photos/3632966/pexels-photo-3632966.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
+  "https://images.pexels.com/photos/4376212/pexels-photo-4376212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=0",
 ];
 
 let a = <Carousel src={images} />;
