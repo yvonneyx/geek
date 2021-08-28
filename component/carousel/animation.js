c1e804ff6f7a7154bf41d43ae2069f69f3e55028
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-28 22:47:59 +0200

    组件化-设计时间线的更新

--- component/carousel/animation.js
+++ component/carousel/animation.js
@@ -15,23 +15,30 @@ let tick = () => {
 const TICK = Symbol("tick");
 const TICK_HANDLER = Symbol("tick-handler");
 const ANIMATIONS = Symbol("animation");
+const START_TIME = Symbol("start-time");
 
 export class Timeline {
   constructor() {
     this[ANIMATIONS] = new Set();
+    this[START_TIME] = new Map();
   }
 
   start() {
     let startTime = Date.now();
     this[TICK] = () => {
-      let t = Date.now() - startTime;
+      let now = Date.now();
       for (let animation of this[ANIMATIONS]) {
-        let t0 = t;
+        let t;
+        if (this[START_TIME].get(animation) < startTime) {
+          t = now - startTime;
+        } else {
+          t = now - this[START_TIME].get(animation);
+        }
         if (animation.duration < t) {
           this[ANIMATIONS].delete(animation);
-          t0 = animation.duration;
+          t = animation.duration;
         }
-        animation.receive(t0);
+        animation.receive(t);
       }
       requestAnimationFrame(this[TICK]);
     };
@@ -46,8 +53,12 @@ export class Timeline {
 
   reset() {}
 
-  add(animation) {
+  add(animation, startTime) {
+    if (arguments.length < 2) {
+      startTime = Date.now();
+    }
     this[ANIMATIONS].add(animation);
+    this[START_TIME].set(animation, startTime);
   }
 
   remove() {}
@@ -60,6 +71,7 @@ export class Animation {
     startValue,
     endValue,
     duration,
+    delay,
     timingFunction
   ) {
     this.object = object;
@@ -68,6 +80,7 @@ export class Animation {
     this.endValue = endValue;
     this.duration = duration;
     this.timingFunction = timingFunction;
+    this.delay = delay;
   }
 
   receive(time) {
--- component/carousel/main.js
+++ component/carousel/main.js
@@ -13,19 +13,18 @@ let images = [
 let a = <Carousel src={images} />;
 a.mountTo(document.body);
 
-let t1 = new Timeline();
-t1.add(
-  new Animation(
-    {
-      set a(v) {
-        console.log(v);
-      },
+let tl = new Timeline();
+window.tl = tl;
+window.animation = new Animation(
+  {
+    set a(v) {
+      console.log(v);
     },
-    "a",
-    0,
-    100,
-    1000,
-    null
-  )
+  },
+  "a",
+  0,
+  100,
+  1000,
+  null
 );
-t1.start();
+tl.start();
