b540b1792d4ee23a1ea98ad70543719f624cfc93
yvonneyx <zhuyuxin0627@gmail.com>
2021-08-28 23:14:41 +0200

    组件化-给动画添加暂停和重启功能

--- component/carousel/animation-demo.js
+++ component/carousel/animation-demo.js
@@ -4,15 +4,20 @@ let tl = new Timeline();
 tl.start();
 tl.add(
   new Animation(
-    {
-      set a(v) {
-        console.log(v);
-      },
-    },
-    "a",
+    document.querySelector("#el").style,
+    "transform",
     0,
-    100,
-    1000,
-    null
+    500,
+    2000,
+    0,
+    null,
+    (v) => `translateX(${v}px)`
   )
 );
+
+document.querySelector("#pause-btn").addEventListener("click", () => {
+  tl.pause();
+});
+document.querySelector("#resume-btn").addEventListener("click", () => {
+  tl.resume();
+});
--- component/carousel/animation.js
+++ component/carousel/animation.js
@@ -16,6 +16,8 @@ const TICK = Symbol("tick");
 const TICK_HANDLER = Symbol("tick-handler");
 const ANIMATIONS = Symbol("animation");
 const START_TIME = Symbol("start-time");
+const PAUSE_START = Symbol("pause-start");
+const PAUSE_TIME = Symbol("pause-time");
 
 export class Timeline {
   constructor() {
@@ -25,14 +27,15 @@ export class Timeline {
 
   start() {
     let startTime = Date.now();
+    this[PAUSE_TIME] = 0;
     this[TICK] = () => {
       let now = Date.now();
       for (let animation of this[ANIMATIONS]) {
         let t;
         if (this[START_TIME].get(animation) < startTime) {
-          t = now - startTime;
+          t = now - startTime - this[PAUSE_TIME];
         } else {
-          t = now - this[START_TIME].get(animation);
+          t = now - this[START_TIME].get(animation) - this[PAUSE_TIME];
         }
         if (animation.duration < t) {
           this[ANIMATIONS].delete(animation);
@@ -40,7 +43,7 @@ export class Timeline {
         }
         animation.receive(t);
       }
-      requestAnimationFrame(this[TICK]);
+      this[TICK_HANDLER] = requestAnimationFrame(this[TICK]);
     };
     this[TICK]();
   }
@@ -48,8 +51,14 @@ export class Timeline {
   //   set rate() {}
   //   get rate() {}
 
-  pause() {}
-  resume() {}
+  pause() {
+    this[PAUSE_START] = Date.now();
+    cancelAnimationFrame(this[TICK_HANDLER]);
+  }
+  resume() {
+    this[PAUSE_TIME] += Date.now() - this[PAUSE_START];
+    this[TICK]();
+  }
 
   reset() {}
 
@@ -72,7 +81,8 @@ export class Animation {
     endValue,
     duration,
     delay,
-    timingFunction
+    timingFunction,
+    template
   ) {
     this.object = object;
     this.property = property;
@@ -81,12 +91,14 @@ export class Animation {
     this.duration = duration;
     this.timingFunction = timingFunction;
     this.delay = delay;
+    this.template = template;
   }
 
   receive(time) {
     let range = this.endValue - this.startValue;
 
-    this.object[this.property] =
-      this.startValue + (range * time) / this.duration;
+    this.object[this.property] = this.template(
+      this.startValue + (range * time) / this.duration
+    );
   }
 }
