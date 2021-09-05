b3d90b5c82cc0220162b6edae95694641188f0e1
yvonneyx <zhuyuxin0627@gmail.com>
2021-09-05 21:51:01 +0200

    组件化-完善Carousel添加children机制
    两种children机制
    1. 内容型children：就是设置了几个就显示几个
    2. 模版型children：放在children里的不是真正的children而是模版

--- component/carousel/framework.js
+++ component/carousel/framework.js
@@ -8,12 +8,20 @@ export function createElement(type, attributes, ...children) {
   for (let name in attributes) {
     element.setAttribute(name, attributes[name]);
   }
-  for (let child of children) {
-    if (typeof child === "string") {
-      child = new TextWrapper(child);
+
+  let processChildren = (children) => {
+    for (let child of children) {
+      if (typeof child === "object" && child instanceof Array) {
+        processChildren(child);
+        continue;
+      }
+      if (typeof child === "string") {
+        child = new TextWrapper(child);
+      }
+      element.appendChild(child);
     }
-    element.appendChild(child);
-  }
+  };
+  processChildren(children);
   return element;
 }
 
@@ -24,6 +32,9 @@ export class Component {
   constructor() {
     this[ATTRIBUTE] = Object.create(null);
     this[STATE] = Object.create(null);
+  }
+  render() {
+    return this.root;
   }
   setAttribute(name, value) {
     this[ATTRIBUTE][name] = value;
@@ -46,12 +57,18 @@ export class Component {
 
 class ElementWrapper extends Component {
   constructor(type) {
+    super();
     this.root = document.createElement(type);
+  }
+  setAttribute(name, value) {
+    debugger;
+    this.root.setAttribute(name, value);
   }
 }
 
 class TextWrapper extends Component {
   constructor(content) {
+    super();
     this.root = document.createTextNode(content);
   }
 }
--- component/carousel/main.js
+++ component/carousel/main.js
@@ -1,35 +1,42 @@
 import { Component, createElement } from "./framework.js";
-import { Carousel } from "./carousel.js";
-import { Timeline, Animation } from "./animation.js";
+import { Carousel } from "./Carousel.js";
+import { Button } from "./Button.js";
+import { List } from "./List.js";
 
 let images = [
   {
     img:
       "https://images.pexels.com/photos/8678811/pexels-photo-8678811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
     url: "https://u.geekbang.org/lesson/159",
+    title: "Geek1",
   },
   {
     img:
       "https://images.pexels.com/photos/5847383/pexels-photo-5847383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
     url: "https://u.geekbang.org/lesson/159",
+    title: "Geek2",
   },
   {
     img:
       "https://images.pexels.com/photos/6626710/pexels-photo-6626710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
     url: "https://u.geekbang.org/lesson/159",
+    title: "Geek3",
   },
   {
     img:
       "https://images.pexels.com/photos/3632966/pexels-photo-3632966.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
     url: "https://u.geekbang.org/lesson/159",
+    title: "Geek4",
   },
   {
     img:
       "https://images.pexels.com/photos/4376212/pexels-photo-4376212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=375&w=630",
     url: "https://u.geekbang.org/lesson/159",
+    title: "Geek5",
   },
 ];
 
+/*
 let a = (
   <Carousel
     src={images}
@@ -41,20 +48,38 @@ let a = (
     }}
   />
 );
+*/
+
+// let a = <Button>content</Button>;
+
+let a = (
+  <List data={images}>
+    {(record) => {
+      return (
+        <div>
+          <img src={record.img} />
+          <a href={record.url}>{record.title}</a>
+        </div>
+      );
+    }}
+  </List>
+);
 a.mountTo(document.body);
 
-// let tl = new Timeline();
-// window.tl = tl;
-// window.animation = new Animation(
-//   {
-//     set a(v) {
-//       console.log(v);
-//     },
-//   },
-//   "a",
-//   0,
-//   100,
-//   1000,
-//   null
-// );
-// tl.start();
+/*
+let tl = new Timeline();
+window.tl = tl;
+window.animation = new Animation(
+  {
+    set a(v) {
+      console.log(v);
+    },
+  },
+  "a",
+  0,
+  100,
+  1000,
+  null
+);
+tl.start();
+*/
